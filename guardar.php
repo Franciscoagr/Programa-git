87255e592d9d2b2919ddba8dbebef36484c0e7cd
francisco <you@example.com>
2023-11-18 17:36:29 -0500

    puesta en marcha del codigo php funcional conectado con base de datos

--- guardar.php
+++ guardar.php
@@ -1,15 +1,14 @@
 <?php
-// Habilita mensajes de error para depuración
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 // Información de conexión a la base de datos
 $db_host = 'localhost:3308';
-$db_user = 'root'; // Reemplaza con tu nombre de usuario de la base de datos
-$db_password = ''; // Reemplaza con tu contraseña de la base de datos
+$db_user = 'root'; 
+$db_password = ''; 
 $db_name = 'proyecto';
 
-// Crea una conexión a la base de datos
+// Crea conexión a la base de datos
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
 // Verifica la conexión
@@ -34,7 +33,6 @@ if ($stmt->execute()) {
     echo "Error: " . $stmt->error;
 }
 
-// Cierra la conexión a la base de datos
 $stmt->close();
 $conn->close();
 ?>
